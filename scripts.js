195696e0297940105130d6bb35a45dbc67e4d611
Cabacon1asu <CABacon1@asu.edu>
2024-12-05 16:14:39 -0500

    Gave up on Cart

--- scripts.js
+++ scripts.js
@@ -1,2 +1,11 @@
 console.log("hey")
 
+let removeCartItemButtons = document.getElementsByClassName('btn-danger')
+console.log(removeCartItemButtons)
+for (let i = 0; i < removeCartItemButtons.length; i++) {
+    let button = removeCartItemButtons[i]
+    button.addEventListener('click', function (event) {
+        let buttonClicked = event.target
+        buttonClicked.getElementsByClassName('cart-row').remove()
+    })
+}
