e743af9f9acccfce2bccd32d4cc2b5c53c1aa23e
Cabacon1asu <CABacon1@asu.edu>
2024-12-06 11:34:57 -0500

    Shopping Cart done

--- scripts.js
+++ scripts.js
@@ -64,3 +64,55 @@ const initApp = () => {
     })
 }
 initApp()
+
+
+const addToCard = (key) => {
+    if(listCards[key] == null){
+        listCards[key] = JSON.parse(JSON.stringify(products[key]));
+        listCards[key].quantity = 1
+    }
+
+    reloadCard();
+}
+
+const reloadCard = () => {
+    listCard.innerHTML = "";
+    let count = 0;
+    let totalPrice = 0;
+
+    listCards.forEach((value,key) => {
+        totalPrice = totalPrice + value.price;
+        count = count + value.quantity;
+
+        if(value != null) {
+            let newDiv = document.createElement("li");
+            newDiv.innerHTML =`
+                <div><img src ="Images/${value.images}"></div>
+                <div class ="cardTitle">${value.name}</div>
+                <div class ="cardPrice">${value.price.toLocaleString()}</div>
+
+                <div>
+                    <button style="background-color: #bde5eb" 
+                    class="cardButton" onclick = "changeQuantity(${key}, ${value.quantity - 1})"></button>
+                    <div class ="count">${count}</div>
+                    <button style="background-color: #bde5eb" 
+                    class="cardButton" onclick = "changeQuantity(${key}, ${value.quantity + 1})"></button>
+                </div>
+            `
+            listCard.appendChild(newDiv);
+        }
+        total.innerText = totalPrice.toLocaleString();
+        quantity.innerText = count;
+    })
+}
+
+const changeQuantity = (key, quantity) => {
+    if(quantity == 0){
+        delete listCards[key]
+    }else{
+        listCards[key].quantity = quantity;
+        listCards[key].price = quantity * products[key].price;
+    }
+    
+    reloadCard();
+}
